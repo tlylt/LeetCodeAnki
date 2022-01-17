6d84cb5d6c3b3a91d4b1a3e06992263960335ea4
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-01-17 15:54:49 +0800

    Time: 155 ms (29.36%), Space: 14.5 MB (12.35%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -1,16 +1,16 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        prefix = self.pattern(s)
-        if prefix[-1] != 0 and len(s) % (len(s) - prefix[-1]) == 0:
+        ans = self.pattern(s)
+        if ans[-1] != 0 and len(s) %(len(s) - ans[-1]) == 0:
             return True
         return False
     def pattern(self, s):
-        prefix = [0] * len(s)
+        ans = [0] * len(s)
         l = 0
         for r in range(1, len(s)):
             while l > 0 and s[l] != s[r]:
-                l = prefix[l-1]
+                l = ans[l-1]
             if s[l] == s[r]:
                 l+=1
-                prefix[r] = l
-        return prefix
+                ans[r] = l
+        return ans
