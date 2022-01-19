46d8c63d6580b3c0138203c5e52ce07a19b17753
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-01-19 15:11:14 +0800

    Time: 43 ms (46.77%), Space: 14.4 MB (37.11%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -2,24 +2,25 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         result = []
         def backtrack(s, startIdx, points):
-            if points == 3:
-                if self.isValid(s, startIdx, len(s)-1):
-                    result.append(s)
+            if points == 3 and self.check(s, startIdx, len(s)-1):
+                result.append(s)
+                return
+            if points >= 3:
                 return
             for i in range(startIdx, len(s)):
-                if self.isValid(s, startIdx, i):
-                    s = s[:i+1] + '.' + s[i+1:]
-                    points+=1
-                    backtrack(s, i+2, points)
-                    points-=1
-                    s = s[:i+1]+ s[i+2:]
+                if not self.check(s, startIdx, i):
+                    continue
+                s = s[:i+1] + '.' + s[i+1:]
+                backtrack(s, i+2, points+1)
+                s = s[:i+1] + s[i+2:]
         backtrack(s, 0, 0)
         return result
-    def isValid(self, s, start, end):
+
+    def check(self, s, start, end):
         if start > end:
             return False
         if s[start] == '0' and start != end:
             return False
-        if not 0<= int(s[start:end+1]) <= 255:
+        if not (0 <= int(s[start:end+1]) <= 255):
             return False
         return True
