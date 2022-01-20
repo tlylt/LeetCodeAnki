c6fb97f516800e46a6e8669f30e6de9f6186db99
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-01-20 20:05:34 +0800

    Time: 32 ms (89.29%), Space: 14.4 MB (8.38%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -1,4 +1,12 @@
 class Solution:
+    def check(self, s, start, end):
+        if start > end:
+            return False
+        if s[start] == '0' and start != end:
+            return False
+        if not (0 <= int(s[start:end+1]) <= 255):
+            return False
+        return True
     def restoreIpAddresses(self, s: str) -> List[str]:
         result = []
         def backtrack(s, startIdx, points):
@@ -8,19 +16,9 @@ class Solution:
             if points >= 3:
                 return
             for i in range(startIdx, len(s)):
-                if not self.check(s, startIdx, i):
-                    continue
-                s = s[:i+1] + '.' + s[i+1:]
-                backtrack(s, i+2, points+1)
-                s = s[:i+1] + s[i+2:]
+                if self.check(s, startIdx, i):
+                    s = s[:i+1] + '.' + s[i+1:]
+                    backtrack(s, i+2, points+1)
+                    s = s[:i+1] + s[i+2:]
         backtrack(s, 0, 0)
         return result
-
-    def check(self, s, start, end):
-        if start > end:
-            return False
-        if s[start] == '0' and start != end:
-            return False
-        if not (0 <= int(s[start:end+1]) <= 255):
-            return False
-        return True
