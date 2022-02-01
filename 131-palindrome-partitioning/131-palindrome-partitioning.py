b11c3712da32a09f76617f69320190b874aac1cc
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-01 13:18:28 +0800

    Time: 1113 ms (24.14%), Space: 30.4 MB (41.23%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,13 +1,4 @@
 class Solution:
-    def check(self, s):
-        l = 0
-        r = len(s) - 1
-        while l < r:
-            if s[l] != s[r]:
-                return False
-            l+=1
-            r-=1
-        return True
     def partition(self, s: str) -> List[List[str]]:
         result = []
         path = []
@@ -16,9 +7,12 @@ class Solution:
                 result.append(path[:])
                 return
             for i in range(startIdx, len(s)):
-                if self.check(s[startIdx:i+1]):
-                    path.append(s[startIdx:i+1])
+                if self.helper(s[startIdx: i+1]):
+                    path.append(s[startIdx: i+1])
                     backtrack(s, i+1)
                     path.pop()
         backtrack(s, 0)
         return result
+    
+    def helper(self, s):
+        return s == s[::-1]
