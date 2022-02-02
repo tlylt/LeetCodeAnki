c82cda99e007284b522bf5140cf51f9027d1c039
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-02 10:30:40 +0800

    Time: 128 ms (38.89%), Space: 14.4 MB (28.78%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -1,17 +1,17 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        s = self.pattern(s)
-        if s[-1] != 0 and len(s) % (len(s) - s[-1]) == 0:
+        p = self.pattern(s)
+        if p[-1] != 0 and len(s) % (len(s)- p[-1]) == 0:
             return True
         return False
-        
     def pattern(self, s):
-        result = [0] * len(s)
+        ans = [0] * len(s)
         l = 0
         for r in range(1, len(s)):
-            while l > 0 and s[l] != s[r]:
-                l = result[l-1]
+            while l > 0 and s[r] != s[l]:
+                l = ans[l-1]
             if s[r] == s[l]:
-                result[r] = l+1
+                ans[r] = l+1
                 l+=1
-        return result
+        return ans
+            
