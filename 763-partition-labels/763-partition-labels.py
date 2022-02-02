7ca214538eb76ebc9ba9635134b8218a95fbd62d
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-02 12:24:06 +0800

    Time: 48 ms (54.20%), Space: 13.7 MB (99.89%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -4,11 +4,11 @@ class Solution:
         for i in range(len(s)):
             h[s[i]] = i
         right = 0
-        ans = []
         l = 0
+        ans = []
         for r in range(len(s)):
             right = max(right, h[s[r]])
             if right == r:
                 ans.append(r-l+1)
-                l = r+1
+                l=r+1
         return ans
