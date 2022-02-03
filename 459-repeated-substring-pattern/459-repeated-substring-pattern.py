0a1228e89491881b10921e1fc8d92ff97a227873
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-03 10:19:43 +0800

    Time: 230 ms (24.13%), Space: 14.6 MB (11.09%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -1,11 +1,12 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         p = self.pattern(s)
-        if p[-1] != 0 and len(s) % (len(s)- p[-1]) == 0:
+        if p[-1] != 0 and len(s) %(len(s)-p[-1]) == 0:
             return True
         return False
+    
     def pattern(self, s):
-        ans = [0] * len(s)
+        ans = [0]*len(s)
         l = 0
         for r in range(1, len(s)):
             while l > 0 and s[r] != s[l]:
@@ -14,4 +15,3 @@ class Solution:
                 ans[r] = l+1
                 l+=1
         return ans
-            
