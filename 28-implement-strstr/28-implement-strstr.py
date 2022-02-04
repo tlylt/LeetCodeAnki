6601f934f10f90ea6f8501b4907e93d537250879
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-04 20:14:52 +0800

    Time: 64 ms (47.71%), Space: 15.2 MB (7.23%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,27 +1,25 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if len(needle) > len(haystack):
-            return -1
-        if len(needle) == 0:
+        if not needle:
             return 0
         p = self.pattern(needle)
-        l = 0 
+        l = 0
         for r in range(len(haystack)):
-            while l > 0 and haystack[r] != needle[l]:
+            while l > 0 and needle[l] != haystack[r]:
                 l = p[l-1]
             if haystack[r] == needle[l]:
                 l+=1
-            if l >= len(p):
-                return r-len(needle)+1
+            if l >= len(needle):
+                return r - l + 1
         return -1
-            
+    
     def pattern(self, s):
-        l = 0
         ans = [0] * len(s)
+        l = 0
         for r in range(1, len(s)):
             while l > 0 and s[l] != s[r]:
                 l = ans[l-1]
-            if s[l] == s[r]:
+            if s[r] == s[l]:
+                ans[r] = l+1
                 l+=1
-                ans[r] = l
         return ans
