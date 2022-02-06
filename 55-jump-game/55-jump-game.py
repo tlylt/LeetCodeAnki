2932efccea5e2bf142e21ae4460917e90bf293ac
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-06 10:36:02 +0800

    Time: 1082 ms (16.88%), Space: 15.4 MB (35.50%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,10 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         cover = 0
-        i = 0
-        while i < len(nums) and i<=cover:
-            cover = max(cover, i+nums[i])
+        for i in range(len(nums)):
+            cover = max(cover, nums[i]+i)
             if cover >= len(nums)-1:
                 return True
-            i+=1
+            if cover <= i:
+                return False
         return False
