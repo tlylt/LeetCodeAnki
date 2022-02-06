aff4b8aac8bdfd6fa6d329bcefb29015a163e85f
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-06 10:56:04 +0800

    Time: 237 ms (24.75%), Space: 14.3 MB (51.60%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -4,14 +4,13 @@ class Solution:
         if p[-1] != 0 and len(s) %(len(s)-p[-1]) == 0:
             return True
         return False
-    
     def pattern(self, s):
-        ans = [0]*len(s)
+        ans = [0] * len(s)
         l = 0
         for r in range(1, len(s)):
-            while l > 0 and s[r] != s[l]:
+            while l > 0 and s[l] != s[r]:
                 l = ans[l-1]
-            if s[r] == s[l]:
+            if s[l] == s[r]:
                 ans[r] = l+1
                 l+=1
         return ans
