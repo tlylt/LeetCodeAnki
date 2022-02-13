570eba5c587fa8002865fb5a08e37952088c42ff
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-13 22:11:58 +0800

    Time: 87 ms (54.51%), Space: 14.5 MB (64.69%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         result = []
-        path = []
+        path =[]
         used = [0] * len(nums)
+        nums.sort()
         def backtrack(nums):
             if len(path) == len(nums):
                 result.append(path[:])
@@ -11,7 +11,7 @@ class Solution:
             for i in range(len(nums)):
                 if i > 0 and nums[i] == nums[i-1] and used[i-1] == 0:
                     continue
-                if used[i] == 1:
+                if used[i]:
                     continue
                 path.append(nums[i])
                 used[i] = 1
