8e7c8a7ee4dd14d718d0e445064b0606cae3a238
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-20 10:22:02 +0800

    Time: 58 ms (73.48%), Space: 18.6 MB (69.20%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -6,9 +6,7 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        ans = self.helper(root)
-        return False if ans == -1 else True
-        
+        return False if self.helper(root) == -1 else True
     def helper(self, node):
         if not node:
             return 0
