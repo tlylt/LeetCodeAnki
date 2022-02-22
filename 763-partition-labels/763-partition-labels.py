58757ecbaa7582228264b85fbd2e7c5b1609daa9
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-22 14:55:10 +0800

    Time: 58 ms (49.09%), Space: 13.9 MB (90.19%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,14 +1,14 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        ans = []
-        left = 0
-        right = 0
         h = {}
         for i in range(len(s)):
             h[s[i]] = i
+        right = 0
+        l = 0
+        result = []
         for j in range(len(s)):
-            right = max(h[s[j]], right)
+            right = max(right, h[s[j]])
             if right == j:
-                ans.append(right-left+1)
-                left = right+1
-        return ans
+                result.append(j-l+1)
+                l = j+1
+        return result
