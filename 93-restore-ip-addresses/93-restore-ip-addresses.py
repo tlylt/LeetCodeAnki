b931ed978729108c548a9025fb3b8dd76d540e5e
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-02-25 16:15:49 +0800

    Time: 75 ms (13.79%), Space: 14 MB (71.60%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -13,8 +13,6 @@ class Solution:
             if points == 3 and self.check(s, startIdx, len(s)-1):
                 result.append(s)
                 return
-            if points >= 3:
-                return
             for i in range(startIdx, len(s)):
                 if self.check(s, startIdx, i):
                     s = s[:i+1] + '.' + s[i+1:]
