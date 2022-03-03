9e892688091367e4b3cbcac6a431f764c6a5c997
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-03 23:18:08 +0800

    Time: 1613 ms (36.49%), Space: 13.9 MB (96.73%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,27 +1,28 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        result = []
         nums.sort()
+        ans = []
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i+1, len(nums)):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                x = j+1
-                y = len(nums) - 1
-                while x < y:
-                    temp = nums[i] + nums[j] + nums[x] + nums[y]
+                l = j+1
+                r =len(nums) - 1
+                while l < r:
+                    temp = nums[i] + nums[j] + nums[l] + nums[r]
                     if temp > target:
-                        y-=1
+                        r -= 1
                     elif temp < target:
-                        x+=1
+                        l += 1
                     else:
-                        result.append([nums[i], nums[j], nums[x], nums[y]])
-                        while x < y and nums[x] == nums[x+1]:
-                            x+=1
-                        while x < y and nums[y] == nums[y-1]:
-                            y-=1
-                        x+=1
-                        y-=1
-        return result
+                        ans.append([nums[i], nums[j], nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l+1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r-1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
+        return ans
+                
