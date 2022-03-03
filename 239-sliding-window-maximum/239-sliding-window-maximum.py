40588bd28b2027d650a0d3995117362bd7ac2000
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-03 21:27:20 +0800

    Time: 2404 ms (56.80%), Space: 30.6 MB (30.50%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,25 +1,25 @@
 class Q:
     def __init__(self):
         self.s = deque()
-    def top(self):
-        return self.s[0]
-    def push(self, val):
-        while self.s and self.s[-1] < val:
+    def add(self, val):
+        while self.s and val > self.s[-1]:
             self.s.pop()
         self.s.append(val)
     def pop(self, val):
         if self.s and self.s[0] == val:
             self.s.popleft()
+    def top(self):
+        return self.s[0]
+    
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         q = Q()
         for i in range(k):
-            q.push(nums[i])
+            q.add(nums[i])
         ans = []
         ans.append(q.top())
         for i in range(k, len(nums)):
-            q.push(nums[i])
+            q.add(nums[i])
             q.pop(nums[i-k])
             ans.append(q.top())
         return ans
-    
