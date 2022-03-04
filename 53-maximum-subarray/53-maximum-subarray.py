daf50d94c873245017c6c241108ace7e7f4a87fc
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-04 22:35:35 +0800

    Time: 3086 ms (5.01%), Space: 28 MB (65.13%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,27 +1,27 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         return self.helper(nums, 0, len(nums)-1)
-    
     def helper(self, nums, l, r):
-        if l == r:
+        if r == l:
             return nums[l]
-        mid = (l+r)//2
-        left = self.helper(nums, l, mid)
-        right = self.helper(nums, mid+1, r)
-        cross = self.crossSum(nums, l, r, mid)
-        return max(left, right, cross)
+        mid = (r+l)//2
+        l_max = self.helper(nums, l, mid)
+        r_max = self.helper(nums, mid+1, r)
+        cross_max = self.cross(nums, l, r, mid)
+        return max(l_max, r_max, cross_max)
     
-    def crossSum(self, nums, l, r, mid):
-        l_sum = 0
+    def cross(self, nums, l, r, mid):
         l_max = float('-inf')
+        l_sum = 0
         for i in range(mid, l-1, -1):
             l_sum += nums[i]
             if l_sum > l_max:
                 l_max = l_sum
-        r_sum = 0
         r_max = float('-inf')
+        r_sum = 0
         for j in range(mid+1, r+1):
             r_sum += nums[j]
             if r_sum > r_max:
                 r_max = r_sum
         return l_max + r_max
+        
