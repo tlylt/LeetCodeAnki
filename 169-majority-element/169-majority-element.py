15b61911b8fb38125b7d7041273b19f72541feef
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-04 22:19:22 +0800

    Time: 312 ms (14.48%), Space: 15.6 MB (33.56%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,18 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return self.helper(nums, 0, len(nums)-1)
+        return self.helper(nums,0, len(nums)-1)
     def helper(self, nums, l, r):
-        if l == r:
+        if r == l:
             return nums[l]
-        mid = (l+r)//2
-        l_major = self.helper(nums, l, mid)
-        r_major = self.helper(nums, mid+1, r)
-        
-        if l_major == r_major:
-            return l_major
+        mid = (r+l)//2
+        left_maj = self.helper(nums, l, mid)
+        right_maj = self.helper(nums, mid+1, r)
+        if left_maj == right_maj:
+            return left_maj
         else:
-            count_l_major = sum(1 for i in range(l, r+1) if nums[i] == l_major)
-            count_r_major = sum(1 for i in range(l, r+1) if nums[i] == r_major)
-            if count_l_major > count_r_major:
-                return l_major
-            return r_major
+            left_count = sum(1 for i in range(l, r+1) if nums[i] == left_maj)
+            right_count = sum(1 for i in range(l, r+1) if nums[i] == right_maj)
+            if left_count > right_count:
+                return left_maj
+            return right_maj
