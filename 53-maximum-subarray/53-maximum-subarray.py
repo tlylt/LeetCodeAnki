06bf2ebbd68d056432dce79a7c53ce69b047d548
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-06 12:36:24 +0800

    Time: 2867 ms (5.01%), Space: 27.9 MB (63.33%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,26 +2,24 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         return self.helper(nums, 0, len(nums)-1)
     def helper(self, nums, l, r):
-        if r == l:
+        if l == r:
             return nums[l]
         mid = (r+l)//2
-        l_max = self.helper(nums, l, mid)
-        r_max = self.helper(nums, mid+1, r)
-        cross_max = self.cross(nums, l, r, mid)
-        return max(l_max, r_max, cross_max)
-    
+        l_sum = self.helper(nums, l, mid)
+        r_sum = self.helper(nums, mid+1, r)
+        cross_sum = self.cross(nums, l, r, mid)
+        return max(l_sum, r_sum, cross_sum)
     def cross(self, nums, l, r, mid):
-        l_max = float('-inf')
         l_sum = 0
+        l_max = float('-inf')
         for i in range(mid, l-1, -1):
             l_sum += nums[i]
             if l_sum > l_max:
                 l_max = l_sum
-        r_max = float('-inf')
         r_sum = 0
-        for j in range(mid+1, r+1):
-            r_sum += nums[j]
+        r_max = float('-inf')
+        for i in range(mid+1, r+1):
+            r_sum += nums[i]
             if r_sum > r_max:
                 r_max = r_sum
         return l_max + r_max
-        
