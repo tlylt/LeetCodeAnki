1fce7c0fab6995bcdd0a0cae1d1ca7e96e740b5f
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-08 11:53:13 +0800

    Time: 3056 ms (27.01%), Space: 14.3 MB (81.20%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,9 +3,9 @@ class Solution:
         total = sum(nums)
         if total % 2 == 1:
             return False
-        half = total//2
-        dp = [0] * (half + 1)
-        for i in range(len(nums)):
-            for j in range(half, nums[i]-1, -1):
+        target = total // 2
+        dp = [0] * (target + 1)
+        for i in range(1, len(nums)):
+            for j in range(target, nums[i] -1 ,-1):
                 dp[j] = max(dp[j], dp[j-nums[i]] + nums[i])
-        return half == dp[-1]
+        return dp[-1] == target
