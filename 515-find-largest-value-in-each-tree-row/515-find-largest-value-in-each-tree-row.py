aafd5ad9bf4b6667c4f451c14d4c86ba81a4b47a
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-08 09:29:35 +0800

    Time: 44 ms (95.12%), Space: 16.3 MB (58.33%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.py
@@ -6,19 +6,19 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
         if not root:
             return []
         q = deque([root])
+        ans = []
         while q:
             l = len(q)
             temp = float('-inf')
             for i in range(l):
-                curr = q.popleft()
-                temp = max(curr.val, temp)
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    q.append(curr.right)
+                c = q.popleft()
+                temp = max(temp, c.val)
+                if c.left:
+                    q.append(c.left)
+                if c.right:
+                    q.append(c.right)
             ans.append(temp)
         return ans
