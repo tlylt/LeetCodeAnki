013551cb81f66311c3794a24e2473ccd04dc7ace
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-08 09:54:20 +0800

    Time: 296 ms (19.61%), Space: 15.5 MB (78.72%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -4,14 +4,13 @@ class Solution:
     def helper(self, nums, l, r):
         if l == r:
             return nums[l]
-        mid = (r+l)//2
+        mid = (r+l) // 2
         l_maj = self.helper(nums, l, mid)
         r_maj = self.helper(nums, mid+1, r)
         if l_maj == r_maj:
             return l_maj
-        else:
-            l_count = sum(1 for i in range(l, r+1) if nums[i] == l_maj)
-            r_count = sum(1 for i in range(l, r+1) if nums[i] == r_maj)
-            if l_count > r_count:
-                return l_maj
-            return r_maj
+        count_l = sum([1 for i in range(l, r+1) if nums[i] == l_maj])
+        count_r = sum([1 for i in range(l, r+1) if nums[i] == r_maj])
+        if count_l > count_r:
+            return l_maj
+        return r_maj
