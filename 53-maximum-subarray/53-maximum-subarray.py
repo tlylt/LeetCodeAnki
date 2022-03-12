ef44d742ad74a6ddecc2a2a5f098593502915ba0
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-12 09:22:15 +0800

    Time: 2427 ms (5.01%), Space: 27.9 MB (86.38%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,25 +1,29 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         return self.helper(nums, 0, len(nums)-1)
+    
     def helper(self, nums, l, r):
         if l == r:
             return nums[l]
-        mid = (r+l)//2
-        l_sum = self.helper(nums, l, mid)
-        r_sum = self.helper(nums, mid+1, r)
-        cross_sum = self.cross(nums, l, r, mid)
-        return max(l_sum, r_sum, cross_sum)
+        mid = (l+r)//2
+        ls = self.helper(nums, l, mid)
+        rs = self.helper(nums, mid+1, r)
+        cs = self.cross(nums, l, r, mid)
+        return max(ls, rs, cs)
+        
     def cross(self, nums, l, r, mid):
-        l_sum = 0
         l_max = float('-inf')
+        l_temp = 0
         for i in range(mid, l-1, -1):
-            l_sum += nums[i]
-            if l_sum > l_max:
-                l_max = l_sum
-        r_sum = 0
+            l_temp += nums[i]
+            if l_max < l_temp:
+                l_max = l_temp
+        
         r_max = float('-inf')
-        for i in range(mid+1, r+1):
-            r_sum += nums[i]
-            if r_sum > r_max:
-                r_max = r_sum
+        r_temp = 0
+        for j in range(mid+1, r+1):
+            r_temp += nums[j]
+            if r_max < r_temp:
+                r_max = r_temp
         return l_max + r_max
+            
