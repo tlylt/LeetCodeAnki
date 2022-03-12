15b7b51bd6f0739282ade5fff014fd0c81238ce2
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-12 16:00:56 +0800

    Time: 720 ms (77.00%), Space: 30.5 MB (36.87%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -5,14 +5,12 @@ class Solution:
         def backtrack(s, startIdx):
             if startIdx >= len(s):
                 result.append(path[:])
-                return
             for i in range(startIdx, len(s)):
-                if self.check(s[startIdx:i+1]):
+                if self.check(s[startIdx: i+1]):
                     path.append(s[startIdx:i+1])
                     backtrack(s, i+1)
                     path.pop()
         backtrack(s, 0)
         return result
-    
     def check(self, s):
         return s == s[::-1]
