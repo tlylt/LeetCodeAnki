2f3bea3ca77cc7f802570961e886e09c001a6f6b
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-13 15:21:05 +0800

    Time: 70 ms (77.71%), Space: 14.3 MB (61.67%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -9,14 +9,15 @@ class Solution:
                 result.append(path[:])
                 return
             for i in range(len(nums)):
-                if used[i]:
+                if used[i] == 1:
                     continue
                 if i > 0 and nums[i] == nums[i-1] and used[i-1] == 0:
                     continue
-                path.append(nums[i])
                 used[i] = 1
+                path.append(nums[i])
                 backtrack(nums)
-                used[i] = 0
                 path.pop()
+                used[i] = 0
         backtrack(nums)
         return result
+                    
