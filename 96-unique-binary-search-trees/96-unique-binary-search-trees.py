0398d5b1569e688e3e86096996366488de6e3a17
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-14 09:34:31 +0800

    Time: 46 ms (43.26%), Space: 14 MB (37.36%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -5,4 +5,5 @@ class Solution:
         for i in range(1, n+1):
             for j in range(1, i+1):
                 dp[i] += dp[j-1] * dp[i-j]
-        return dp[-1]
+        print(dp)
+        return dp[n]
