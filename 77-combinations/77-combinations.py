252847c1cba804be1bcbbf2c2dded1cd10d1f322
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-17 10:34:16 +0800

    Time: 607 ms (38.70%), Space: 16 MB (18.12%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -6,10 +6,9 @@ class Solution:
             if len(path) == k:
                 result.append(path[:])
                 return
-            for i in range(startIdx, n-(k-len(path))+2):
+            for i in range(startIdx, n+1):
                 path.append(i)
                 backtrack(n, k, i+1)
                 path.pop()
         backtrack(n, k, 1)
         return result
-                
