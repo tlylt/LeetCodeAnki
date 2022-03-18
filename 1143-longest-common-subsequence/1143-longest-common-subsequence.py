06fbc14ab50e7ea36601fd22778b99bea278f890
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-18 20:09:59 +0800

    Time: 633 ms (48.07%), Space: 22.7 MB (58.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -9,4 +9,4 @@ class Solution:
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[-1][-1]
+        return dp[m][n]
