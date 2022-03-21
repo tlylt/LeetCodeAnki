a39b750a418740e5f146d5703b5f6cddadac6683
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-21 08:22:54 +0800

    Time: 75 ms (22.69%), Space: 13.8 MB (78.97%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,9 +1,9 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        r = 0
         h = {}
         for i in range(len(s)):
             h[s[i]] = i
-        r = 0
         l = 0
         ans = []
         for i in range(len(s)):
@@ -12,4 +12,3 @@ class Solution:
                 ans.append(r-l+1)
                 l = r+1
         return ans
-            
