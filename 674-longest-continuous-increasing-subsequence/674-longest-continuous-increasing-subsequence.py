6b5494df982b3502f8610ff0b74856e5992e9377
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-03-29 10:21:26 +0800

    Time: 92 ms (67.52%), Space: 15.3 MB (86.70%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.py
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.py
@@ -3,5 +3,5 @@ class Solution:
         dp = [1] * len(nums)
         for i in range(1, len(nums)):
             if nums[i] > nums[i-1]:
-                dp[i] = dp[i-1]+1
+                dp[i] = max(dp[i], dp[i-1] + 1)
         return max(dp)
