d6b4190bfededddab45eb97139735f6279610a5f
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-03 14:34:32 +0800

    Time: 1054 ms (58.85%), Space: 14 MB (66.59%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -9,13 +9,13 @@ class Solution:
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
                 l = j+1
-                r =len(nums) - 1
+                r = len(nums)-1
                 while l < r:
                     temp = nums[i] + nums[j] + nums[l] + nums[r]
-                    if temp > target:
-                        r -= 1
-                    elif temp < target:
+                    if temp < target:
                         l += 1
+                    elif temp > target:
+                        r -= 1
                     else:
                         ans.append([nums[i], nums[j], nums[l], nums[r]])
                         while l < r and nums[l] == nums[l+1]:
@@ -25,4 +25,4 @@ class Solution:
                         l += 1
                         r -= 1
         return ans
-                
+            
