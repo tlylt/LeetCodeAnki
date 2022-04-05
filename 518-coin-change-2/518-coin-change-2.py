a21167f3cf30a3243f3dc1b825e6b92fbdb718e7
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-05 09:28:16 +0800

    Time: 168 ms (84.52%), Space: 14 MB (89.87%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,9 +1,8 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount+1)
+        dp = [0] * (amount + 1)
         dp[0] = 1
         for i in range(len(coins)):
-            for j in range(1, amount+1):
-                if coins[i] <= j:
-                    dp[j] += dp[j-coins[i]]
+            for j in range(coins[i], amount+1):
+                dp[j] += dp[j-coins[i]]
         return dp[amount]
