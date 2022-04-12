ef5be2bc2ca9c3c9d7cd2bf3492b8165fbf4c635
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-12 09:14:41 +0800

    Time: 199 ms (66.79%), Space: 16.9 MB (41.89%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -3,8 +3,8 @@ class Solution:
         ans = [1] * len(ratings)
         for i in range(1, len(ratings)):
             if ratings[i] > ratings[i-1]:
-                ans[i] = ans[i-1]+1
+                ans[i] = ans[i-1] + 1
         for j in range(len(ratings)-2, -1, -1):
             if ratings[j] > ratings[j+1]:
-                ans[j] = max(ans[j], ans[j+1] + 1)
+                ans[j] = max(ans[j+1]+1, ans[j])
         return sum(ans)
