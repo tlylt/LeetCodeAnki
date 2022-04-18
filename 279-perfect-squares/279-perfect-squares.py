92a4456e3e7d750b50848da280c8581dc8bdd299
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-18 14:01:28 +0800

    Time: 4182 ms (51.91%), Space: 14.2 MB (51.94%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -3,9 +3,9 @@ class Solution:
         dp = [float('inf')] * (n+1)
         dp[0] = 0
         for i in range(1, n+1):
-            if i*i > n:
+            sq = i * i
+            if sq > n:
                 break
-            for j in range(1, n+1):
-                if j >= i*i:
-                    dp[j] = min(dp[j], dp[j-i*i]+1)
-        return dp[n] if dp[n] != float('inf') else -1
+            for j in range(sq, n+1):
+                dp[j] = min(dp[j], dp[j-sq] + 1)
+        return dp[n] if dp[n] != float('inf') else 0
