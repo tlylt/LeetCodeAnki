b860a2fed5eb0dc8c9770c055d881633d5226e0d
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-19 15:13:33 +0800

    Time: 89 ms (11.03%), Space: 17 MB (26.43%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        depth = -1
+        depth = 0
         ans = 0
         def backtrack(root, curr):
-            nonlocal depth, ans
             if not root:
                 return
+            nonlocal depth, ans
             if curr > depth:
                 depth = curr
                 ans = root.val
             backtrack(root.left, curr+1)
             backtrack(root.right, curr+1)
-        backtrack(root, 0)
+        backtrack(root, 1)
         return ans
