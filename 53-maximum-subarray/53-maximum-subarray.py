cff6e39ca11191da534a8e4afab6730046b7bca2
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-22 09:47:45 +0800

    Time: 2314 ms (5.00%), Space: 28 MB (40.97%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,11 +1,24 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        ans = nums[0]
+        return self.helper(nums, 0, len(nums)-1)
+    def helper(self, nums, l, r):
+        if l == r:
+            return nums[l]
+        mid = (l + r) // 2
+        lm = self.helper(nums, l, mid)
+        rm = self.helper(nums, mid+1, r)
+        cm = self.crossSum(nums, l, r, mid)
+        return max(lm, rm, cm)
+    def crossSum(self, nums, l, r, mid):
+        left = float('-inf')
         temp = 0
-        for i in range(len(nums)):
+        for i in range(mid, l-1, -1):
             temp += nums[i]
-            if temp > ans:
-                ans = temp
-            if temp < 0:
-                temp = 0
-        return ans
+            if temp > left:
+                left = temp
+        temp = 0
+        right = float('-inf')
+        for j in range(mid+1, r+1):
+            temp += nums[j]
+            right = max(temp, right)
+        return left + right
