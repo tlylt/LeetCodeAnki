877463de0f11b6822fa2965220f8bb20cd566a35
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-22 09:28:30 +0800

    Time: 59 ms (24.77%), Space: 13.9 MB (65.33%) - LeetHub

--- 343-integer-break/343-integer-break.py
+++ 343-integer-break/343-integer-break.py
@@ -3,6 +3,6 @@ class Solution:
         dp = [0] * (n+1)
         dp[2] = 1
         for i in range(3, n+1):
-            for j in range(i):
+            for j in range(1, i+1):
                 dp[i] = max(dp[i], j * (i-j), j * dp[i-j])
         return dp[n]
