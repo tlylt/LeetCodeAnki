aa3af5018d88b2427423a69bdf6126f391c89d03
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-22 09:16:18 +0800

    Time: 297 ms (16.77%), Space: 15.5 MB (30.93%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -2,16 +2,16 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         return self.helper(nums, 0, len(nums)-1)
     def helper(self, nums, l, r):
-        if l >= r:
+        if l == r:
             return nums[l]
         mid = (l+r) // 2
         lm = self.helper(nums, l, mid)
         rm = self.helper(nums, mid+1, r)
         if lm == rm:
             return lm
-        lc = sum([1 for i in range(l, r+1) if nums[i] == lm])
-        rc = sum([1 for i in range(l, r+1) if nums[i] == rm])
-        if lc > rc:
-            return lm
         else:
+            lc = sum([1 for i in range(l, r+1) if nums[i] == lm])
+            rc = sum([1 for i in range(l, r+1) if nums[i] == rm])
+            if lc > rc:
+                return lm
             return rm
