88369133c08758ce7eb6747f9983310f27ba7cd9
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-23 10:57:12 +0800

    Time: 6334 ms (29.52%), Space: 40.1 MB (12.17%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -8,5 +8,5 @@ class Solution:
             for j in range(1, n+1):
                 if nums1[i-1] == nums2[j-1]:
                     dp[i][j] = dp[i-1][j-1] + 1
-                result = max(result, dp[i][j])
+                    result = max(result, dp[i][j])
         return result
