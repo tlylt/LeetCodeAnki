fdb9d23681e11ca49dbd7683814f315ff78a4fb2
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-23 17:23:55 +0800

    Time: 264 ms (70.57%), Space: 14.2 MB (87.52%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -2,7 +2,7 @@ class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         m = len(nums1)
         n = len(nums2)
-        dp =[[0 for i in range(n+1)] for j in range(m+1)]
+        dp = [[0 for i in range(n+1)] for j in range(m+1)]
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if nums1[i-1] == nums2[j-1]:
