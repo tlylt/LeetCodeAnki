353b6bf2ad33606871a167037ad51a9f56b38872
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-23 20:26:56 +0800

    Time: 1536 ms (77.95%), Space: 14 MB (96.04%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount+1)
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
         for i in range(len(coins)):
             for j in range(coins[i], amount+1):
