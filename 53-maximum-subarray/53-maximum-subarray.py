5141a3fa47fc0f2ad241eff299498a1ae2c79481
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-24 08:38:57 +0800

    Time: 3375 ms (5.01%), Space: 27.9 MB (41.02%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,21 +4,21 @@ class Solution:
     def helper(self, nums, l, r):
         if l == r:
             return nums[l]
-        mid = (l + r) // 2
-        lm = self.helper(nums, l, mid)
-        rm = self.helper(nums, mid+1, r)
-        cm = self.crossSum(nums, l, r, mid)
-        return max(lm, rm, cm)
-    def crossSum(self, nums, l, r, mid):
-        left = float('-inf')
+        mid = (l+r) // 2
+        ls = self.helper(nums, l, mid)
+        rs = self.helper(nums, mid+1, r)
+        cs = self.cross(nums, l, r, mid)
+        return max(ls, rs, cs)
+    
+    def cross(self, nums, l, r, mid):
+        ls = float('-inf')
         temp = 0
         for i in range(mid, l-1, -1):
             temp += nums[i]
-            if temp > left:
-                left = temp
+            ls = max(temp, ls)
+        rs = float('-inf')
         temp = 0
-        right = float('-inf')
         for j in range(mid+1, r+1):
             temp += nums[j]
-            right = max(temp, right)
-        return left + right
+            rs = max(temp, rs)
+        return ls + rs
