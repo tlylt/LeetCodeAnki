42befbe1b3c85db431677a7c9dc4dc6957b62fee
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-04-26 07:52:14 +0800

    Time: 3806 ms (57.89%), Space: 14.3 MB (15.51%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -4,5 +4,5 @@ class Solution:
         for i in range(1, len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+                    dp[i] = max(dp[i], dp[j]+1)
         return max(dp)
