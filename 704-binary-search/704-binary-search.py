26f924dd0f0162d910eb70187271fa21c375e8c8
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-05-03 23:49:13 +0800

    Time: 372 ms (26.83%), Space: 15.4 MB (74.54%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -4,10 +4,11 @@ class Solution:
     def helper(self, nums, l, r, target):
         if l > r:
             return -1
-        mid = (l+r) // 2
+        mid = (l+r)//2
         if nums[mid] == target:
             return mid
         elif nums[mid] > target:
-            return self.helper(nums, l, mid-1, target)            
+            return self.helper(nums, l, mid-1, target)
         else:
             return self.helper(nums, mid+1, r, target)
+        
