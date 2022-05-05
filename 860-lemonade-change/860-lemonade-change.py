6620117414712a8c0a52bfbd5222bf3ca0086f51
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-05-05 21:52:53 +0800

    Time: 1332 ms (18.47%), Space: 17.8 MB (88.42%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.py
+++ 860-lemonade-change/860-lemonade-change.py
@@ -4,14 +4,14 @@ class Solution:
         for b in bills:
             if b == 5:
                 five += 1
-            elif b == 10:
+            if b == 10:
                 if five > 0:
-                    five -= 1
                     ten += 1
+                    five -= 1
                 else:
                     return False
-            else:
-                if five > 0 and ten > 0:
+            if b == 20:
+                if ten > 0 and five > 0:
                     ten -= 1
                     five -= 1
                 elif five > 2:
