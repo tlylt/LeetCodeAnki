d7406e8513475d2b3faedab61930233d815890b2
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-05-07 21:41:00 +0800

    Time: 5579 ms (32.24%), Space: 14.2 MB (45.58%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -6,7 +6,7 @@ class Solution:
             s = i * i
             if s > n:
                 break
-            for j in range(1, n+1):
-                if j >= s:
-                    dp[j] = min(dp[j], dp[j-s]+1)
+            for i in range(1, n+1):
+                if i >= s:
+                    dp[i] = min(dp[i], dp[i-s] + 1)
         return dp[n]
