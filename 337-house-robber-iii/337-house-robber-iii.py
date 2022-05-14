f082b4cb7c1b7aef6d30f3b1e41b2fadce1b877c
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-05-15 00:01:38 +0800

    Time: 64 ms (61.02%), Space: 16.1 MB (64.06%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.py
+++ 337-house-robber-iii/337-house-robber-iii.py
@@ -6,13 +6,16 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        rob, dontRob = self.helper(root)
-        return max(rob, dontRob)
+        ans = self.helper(root)
+        return max(ans[0], ans[1])
     def helper(self, root):
         if not root:
             return 0, 0
         l = self.helper(root.left)
         r = self.helper(root.right)
-        robV = root.val + l[1] + r[1]
-        dontRobV = max(l[0], l[1]) + max(r[0], r[1])
-        return robV, dontRobV
+        
+        rob = root.val + l[1] + r[1]
+        dontrob = max(l[0], l[1]) + max(r[0], r[1])
+        
+        return rob, dontrob
+            
