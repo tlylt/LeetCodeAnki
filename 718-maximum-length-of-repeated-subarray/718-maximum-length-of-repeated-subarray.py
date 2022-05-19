9ec8a99cf6ff0dc8bd6c5527f3cd60172cff1cfe
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-05-19 09:46:08 +0800

    Time: 6678 ms (23.95%), Space: 40 MB (26.64%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -9,4 +9,4 @@ class Solution:
                 if nums1[i-1] == nums2[j-1]:
                     dp[i][j] = dp[i-1][j-1] + 1
                     ans = max(ans, dp[i][j])
-        return ans                    
+        return ans          
