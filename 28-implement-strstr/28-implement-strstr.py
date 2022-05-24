027767178f23cb355f1dd1b00ad3763b16a56e4e
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-05-24 23:15:36 +0800

    Time: 37 ms (69.45%), Space: 13.9 MB (27.64%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,24 +1,24 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle == '':
+        if needle == "":
             return 0
-        ref = self.helper(needle)
+        h = self.helper(needle)
         l = 0
         for r in range(len(haystack)):
             while l > 0 and haystack[r] != needle[l]:
-                l = ref[l-1]
+                l = h[l-1]
             if haystack[r] == needle[l]:
                 l += 1
             if l >= len(needle):
                 return r - l + 1
         return -1
     def helper(self, s):
-        ans = [0] * len(s)
+        h = [0] * len(s)
         l = 0
         for r in range(1, len(s)):
             while l > 0 and s[l] != s[r]:
-                l = ans[l-1]
+                l = h[l-1]
             if s[l] == s[r]:
-                ans[r] = l + 1
+                h[r] = l+1
                 l += 1
-        return ans
+        return h
