ee73817868af81f9580f4d0f5b3681060a574b10
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-06-06 22:59:01 +0800

    Time: 59 ms (42.85%), Space: 14 MB (71.35%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(1, len(s)+1):
             for w in wordDict:
                 if len(w) <= i:
-                    dp[i] = dp[i] or (dp[i-len(w)] and s[i-len(w):i] == w)
+                    dp[i] = dp[i] or (dp[i-len(w)] and w == s[i-len(w):i])
         return dp[len(s)]
