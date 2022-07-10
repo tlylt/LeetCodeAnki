f4eb2369ca1e0f48f9f10d2269314a254e10101c
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-07-10 16:58:19 +0800

    Time: 313 ms (66.33%), Space: 15.5 MB (22.25%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,17 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return self.helper(nums, 0, len(nums)-1, target)
-    def helper(self, nums, l, r, target):
-        if l > r:
-            return -1
-        if l == r:
-            if nums[l] == target:
-                return l
-            return -1
-        mid = (l+r) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            return self.helper(nums, l, mid-1, target)
-        else:
-            return self.helper(nums, mid+1, r, target)
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
