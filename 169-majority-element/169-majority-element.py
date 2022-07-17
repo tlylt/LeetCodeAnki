65990bafb8adc43a1cbc9883ff539a4ec1e1eeff
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-07-17 10:00:32 +0800

    Time: 293 ms (31.11%), Space: 15.6 MB (34.60%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -9,9 +9,8 @@ class Solution:
         rm = self.helper(nums, mid+1, r)
         if lm == rm:
             return lm
-        else:
-            lc = sum([1 for i in range(l, r+1) if nums[i] == lm])
-            rc = sum([1 for i in range(l, r+1) if nums[i] == rm])
-            if lc > rc:
-                return lm
-            return rm
+        lc = sum([1 for i in range(l, r+1) if nums[i] == lm])
+        rc = sum([1 for i in range(l, r+1) if nums[i] == rm])
+        if lc > rc:
+            return lm
+        return rm
