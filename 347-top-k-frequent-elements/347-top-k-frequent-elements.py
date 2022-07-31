0a5afcbd2c28a65a6fb0000ffed871282ba46787
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-07-31 22:19:05 +0800

    Time: 137 ms (71.76%), Space: 18.9 MB (31.42%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,15 +1,15 @@
 import heapq # min heap
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        h = {}
+        ref = {}
         for i in nums:
-            h[i] = h.get(i, 0) + 1
-        ref = []
-        for ke, va in h.items():
-            heapq.heappush(ref, (va, ke))
-        for i in range(len(ref) - k):
-            heapq.heappop(ref)
+            ref[i] = ref.get(i, 0) + 1
+        h = []
+        for ke, va in ref.items():
+            heapq.heappush(h, (va, ke))
         ans = []
-        for i in ref:
-            ans.append(i[1])
+        for i in range(len(h)-k):
+            heapq.heappop(h)
+        for j in range(k):
+            ans.append(heapq.heappop(h)[1])
         return ans
