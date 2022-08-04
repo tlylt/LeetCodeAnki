d567a50c7af16baaa9890ce1d1b91cd676a8d6ac
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-08-04 18:23:51 +0800

    Time: 45 ms (92.08%), Space: 14.1 MB (43.40%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -13,11 +13,9 @@ class Solution:
             if obstacleGrid[0][j] == 1:
                 flag = 0
             dp[0][j] = flag
-        dp[0][0] = 0 if obstacleGrid[0][0] == 1 else 1
         for i in range(1, m):
             for j in range(1, n):
                 if obstacleGrid[i][j] == 1:
                     continue
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
