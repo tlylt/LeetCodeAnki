469a6f8ff7255fb7b2cc3c027b3a0dfcce4d9299
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-08-08 22:12:16 +0800

    Time: 478 ms (20.69%), Space: 22.6 MB (10.88%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -5,11 +5,10 @@ class Solution:
         for i in range(len(s)-1, -1, -1):
             for j in range(i, len(s)):
                 if s[i] == s[j]:
-                    if abs(j-i) <= 1:
+                    if (j-i) <= 1:
                         ans += 1
                         dp[i][j] = True
-                    else:
-                        if dp[i+1][j-1]:
-                            ans += 1
-                            dp[i][j] = True
+                    elif dp[i+1][j-1]:
+                        ans += 1
+                        dp[i][j] = True
         return ans
