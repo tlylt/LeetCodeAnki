f87b5647a36bb3bf1532d6807146c5a57491e11d
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-09-05 00:36:49 +0800

    Time: 53 ms (49.15%), Space: 13.8 MB (70.31%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -8,10 +8,11 @@ class Solution:
         dummy = ListNode(next=head)
         fast = dummy
         slow = dummy
-        while n >= 0:
-            fast = fast.next
+        while n > 0:
+            if fast:
+                fast = fast.next
             n -= 1
-        while fast:
+        while fast.next:
             fast = fast.next
             slow = slow.next
         slow.next = slow.next.next
