a1eca28f2a3fc5dca15453b4f8d0dcc0414f25a5
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-09-06 11:34:59 +0800

    Time: 287 ms (38.20%), Space: 15.9 MB (44.83%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -3,13 +3,11 @@ class Solution:
         g.sort()
         s.sort()
         ans = 0
-        if len(g) == 0:
-            return 0
         pt = 0
         for i in s:
-            if pt >= len(g):
-                return ans
-            if i >= g[pt]:
+            if pt < len(g) and g[pt] <= i:
                 ans += 1
                 pt += 1
+                continue
         return ans
+                
