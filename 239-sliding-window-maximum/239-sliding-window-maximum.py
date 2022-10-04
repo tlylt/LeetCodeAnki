cf41d63d8636df8cee65baaa395325ebebcfd575
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-10-04 09:45:06 +0800

    Time: 3046 ms (55.12%), Space: 29.3 MB (97.62%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,26 +1,26 @@
 class Q:
     def __init__(self):
-        self.q = deque()
-    def add(self, num):
-        while self.q and self.q[-1] < num:
-            self.q.pop()
-        self.q.append(num)
-    def pop(self, num):
-        if self.q[0] == num:
-            self.q.popleft()
+        self.ref = deque()
+    def add(self, val):
+        while self.ref and self.ref[-1] < val:
+            self.ref.pop()
+        self.ref.append(val)
+    def pop(self, val):
+        if val == self.ref[0]:
+            self.ref.popleft()
     def top(self):
-        return self.q[0]
+        return self.ref[0]
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        q = Q()
+        ref = Q()
         for i in range(k):
-            q.add(nums[i])
+            ref.add(nums[i])
         ans = []
-        ans.append(q.top())
+        ans.append(ref.top())
         l = 0
         for i in range(k, len(nums)):
-            q.add(nums[i])
-            q.pop(nums[l])
+            ref.pop(nums[l])
+            ref.add(nums[i])
+            ans.append(ref.top())
             l += 1
-            ans.append(q.top())
         return ans
