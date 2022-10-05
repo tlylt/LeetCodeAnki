efa3ff7a0080b06776ff57df001c04b39719fe1e
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-10-05 23:56:07 +0800

    Time: 41 ms (84.36%), Space: 14.2 MB (35.70%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -2,16 +2,11 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
         path = []
-        used = [0] * len(nums)
         def backtrack(nums, idx):
             result.append(path[:])
             for i in range(idx, len(nums)):
-                if used[i] == 1:
-                    continue
-                used[i] = 1
                 path.append(nums[i])
                 backtrack(nums, i+1)
                 path.pop()
-                used[i] = 0
         backtrack(nums, 0)
         return result
