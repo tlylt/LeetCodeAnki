543cec2e442655658a3d289f516ca02573c3a7dd
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-10-05 16:44:15 +0800

    Time: 71 ms (41.09%), Space: 14.2 MB (10.53%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -3,10 +3,10 @@ class Solution:
         nums.sort()
         result = []
         path = []
-        def backtrack(nums, startIdx):
+        def backtrack(nums, idx):
             result.append(path[:])
-            for i in range(startIdx, len(nums)):
-                if i > startIdx and nums[i] == nums[i-1]:
+            for i in range(idx, len(nums)):
+                if i > idx and nums[i] == nums[i-1]:
                     continue
                 path.append(nums[i])
                 backtrack(nums, i+1)
