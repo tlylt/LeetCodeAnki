066547ce21436c6b0a62807229b8a3612f70b484
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-10-11 16:21:45 +0800

    Time: 87 ms (5.01%), Space: 14.9 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -8,5 +8,5 @@ class Solution:
                 if s[i-1] == t[j-1]:
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                    dp[i][j] = dp[i][j-1]
         return dp[m][n] == len(s)
