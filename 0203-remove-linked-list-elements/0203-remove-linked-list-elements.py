2f49fa8782de6f3e376cd25b028dcf9f211174a4
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2023-01-18 21:29:18 +0800

    Time: 66 ms (92.20%), Space: 17.9 MB (35.54%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head:
+            return None
+        dummy = ListNode(next = head)
+        prev = dummy
+        curr = head
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        return dummy.next
