038af9eba4f7b2d2d012f7452c43bc72cfb81f98
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2023-01-19 12:40:24 +0800

    Time: 53 ms (75.45%), Space: 16 MB (99.89%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        ans = self.helper(root)
+        return max(ans[0], ans[1])
+    def helper(self, root):
+        if not root:
+            return 0, 0
+        l = self.helper(root.left)
+        r = self.helper(root.right)
+        rob = root.val + l[1] + r[1]
+        dontrob = max(l[0], l[1]) + max(r[0], r[1])
+        return rob, dontrob
