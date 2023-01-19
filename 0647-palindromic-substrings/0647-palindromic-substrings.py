58cd13a71d3aba9e125f04b942446d0d7e7807b2
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2023-01-19 13:21:05 +0800

    Time: 100 ms (96.82%), Space: 13.9 MB (72.11%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,14 +1,15 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        dp = [[False for i in range(len(s))] for j in range(len(s))]
+        # dp = [[False for i in range(len(s))] for j in range(len(s))]
         ans = 0
-        for i in range(len(s)-1, -1, -1):
-            for j in range(i, len(s)):
-                if s[i] == s[j]:
-                    if j - i <= 1:
-                        ans += 1
-                        dp[i][j] = True
-                    elif dp[i+1][j-1]:
-                        ans += 1
-                        dp[i][j] = True
+        for i in range(len(s)):
+            ans += self.helper(s, i, i, len(s))
+            ans += self.helper(s, i, i+1, len(s))
+        return ans
+    def helper(self, s, i, j, n):
+        ans = 0
+        while i >= 0 and j < n and s[i] == s[j]:
+            ans += 1
+            i -= 1
+            j += 1
         return ans
