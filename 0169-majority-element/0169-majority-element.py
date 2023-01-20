7b5a4593485c135eff84bc788cda1eec93fcab32
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2023-01-20 22:32:23 +0800

    Time: 274 ms (34.98%), Space: 15.5 MB (30.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,13 +4,13 @@ class Solution:
     def helper(self, nums, l, r):
         if l == r:
             return nums[l]
-        mid = l + (r-l) // 2
+        mid = l + (r-l)//2
         lm = self.helper(nums, l, mid)
         rm = self.helper(nums, mid+1, r)
         if lm == rm:
             return lm
         lc = sum([1 for i in range(l, r+1) if nums[i] == lm])
-        rc = sum([1 for i in range(l, r+1) if nums[i] == rm])
+        rc = sum([1 for j in range(l, r+1) if nums[j] == rm])
         if lc > rc:
             return lm
         return rm
