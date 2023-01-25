acd9c3c80ac3e1984483bbc92694fcf4a800bac7
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-01-25 14:10:32 +0800

    Time: 88 ms (93.76%), Space: 15.9 MB (84.98%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -6,6 +6,8 @@ class Solution:
             if len(path) == k:
                 result.append(path[:])
                 return
+            if len(path) > k:
+                return
             for i in range(idx, n-(k-len(path))+2):
                 path.append(i)
                 backtrack(n, k, i+1)
