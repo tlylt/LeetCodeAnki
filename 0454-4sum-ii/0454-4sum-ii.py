96dad114fab36a1ec471e329e9f7b9aa03c64a97
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-02-06 11:21:35 +0800

    Time: 956 ms (33.09%), Space: 14.2 MB (25.71%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,12 +1,11 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        ref = {}
         ans = 0
-        for i in nums1:
-            for j in nums2:
-                ref[i+j] = ref.get(i+j, 0) + 1
-        for x in nums3:
-            for y in nums4:
-                if (0-x-y) in ref:
-                    ans += ref[0-x-y]
+        ref = {}
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                ref[0 - (nums1[i] + nums2[j])] = ref.get((0-(nums1[i] + nums2[j])), 0) + 1
+        for x in range(len(nums3)):
+            for y in range(len(nums4)):
+                ans += ref.get(nums3[x] + nums4[y], 0)
         return ans
