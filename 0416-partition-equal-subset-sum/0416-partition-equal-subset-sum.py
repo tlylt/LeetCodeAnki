4ce2d1b5660332fc6262c48dc0556c442332612c
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-02-06 11:26:44 +0800

    Time: 4176 ms (16.15%), Space: 14.2 MB (78.58%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -6,7 +6,6 @@ class Solution:
         target = total // 2
         dp = [0] * (target+1)
         for i in range(len(nums)):
-            for j in range(target, 0, -1):
-                if nums[i] <= j:
-                    dp[j] = max(dp[j], dp[j-nums[i]] + nums[i])
+            for j in range(target, nums[i]-1, -1):
+                dp[j] = max(dp[j], dp[j-nums[i]] + nums[i])
         return dp[target] == target
