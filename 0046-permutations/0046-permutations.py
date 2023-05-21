ad5c7a5b8900d46fe6ef2d2ac088474b0d91d136
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-05-21 15:13:04 +0800

    Time: 59 ms (14.75%), Space: 16.6 MB (11.20%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,17 +3,17 @@ class Solution:
         result = []
         path = []
         used = [0] * len(nums)
-        def backtrack():
+        def backtrack(nums):
             if len(path) == len(nums):
                 result.append(path[:])
+                return
             for i in range(len(nums)):
                 if used[i] == 1:
                     continue
                 used[i] = 1
                 path.append(nums[i])
-                backtrack()
+                backtrack(nums)
                 path.pop()
                 used[i] = 0
-        backtrack()
+        backtrack(nums)
         return result
-                            
