1d2f0483fe3902f00761ef585272c75e224a264d
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-07-09 09:06:37 +0800

    Time: 49 ms (50.33%), Space: 16.4 MB (10.41%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,25 +1,25 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        u, d, l, r = 0, n-1, 0, n-1
-        x = 1
-        ans = [[0] * n for i in range(n)]
-        while u < d and l < r:
+        u, d, l, r = 0, n - 1, 0, n - 1
+        curr = 1
+        ans = [[0] * n for i in range(n) ]
+        while l < r and u < d:
             for i in range(l, r):
-                ans[u][i] = x
-                x += 1
+                ans[u][i] = curr
+                curr += 1
             for j in range(u, d):
-                ans[j][r] = x
-                x += 1
+                ans[j][r] = curr
+                curr += 1
             for k in range(r, l, -1):
-                ans[d][k] = x
-                x += 1
+                ans[d][k] = curr
+                curr += 1
             for z in range(d, u, -1):
-                ans[z][l] = x
-                x += 1
+                ans[z][l] = curr
+                curr += 1
             l += 1
             r -= 1
             u += 1
             d -= 1
         if n % 2 == 1:
-            ans[n // 2][n // 2] = x
+            ans[n//2][n//2] = curr
         return ans
