939d6084672543938e16337f919e7f69cd37991f
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-07-13 21:41:34 +0800

    Time: 63 ms (71.91%), Space: 16.5 MB (11.76%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        if not words or len(words) == 0:
+            return []
+        ref = [0] * 26
+        for i in words[0]:
+            ref[ord(i)-ord('a')] += 1
+        
+        for j in range(1, len(words)):
+            ref2 = [0] * 26
+            for k in words[j]:
+                ref2[ord(k)-ord('a')] += 1
+            for x in range(26):
+                ref[x] = min(ref[x], ref2[x])
+        ans = []
+        for y in range(26):
+            while ref[y] != 0:
+                ans.append(chr(ord('a') + y))
+                ref[y] -= 1
+        return ans
+            
