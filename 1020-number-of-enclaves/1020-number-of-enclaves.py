e985df74687f8235fd6ba5196a9d09644bcd1ca3
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2023-10-04 23:01:07 +0800

    Time: 580 ms (51.66%), Space: 91.2 MB (25.60%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,37 +1,50 @@
 class Solution:
-    def bfs(self, grid, queue, visited):
-        positions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        while queue:
-            curr = queue.popleft()
-            for pos in positions:
-                row, col = curr[0] + pos[0], curr[1] + pos[1]
-                if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
-                    continue
-                if grid[row][col] == 0 or visited[row][col]:
-                    continue
-                visited[row][col] = True
-                queue.append([row, col])
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        row_len, col_len, ans = len(grid), len(grid[0]), 0
-        visited = [[False for _ in range(col_len)] for _ in range(row_len)]
-        queue = deque()
-        for row in range(row_len):
-            if grid[row][0] == 1:
-                visited[row][0] = True
-                queue.append((row, 0))
-            if grid[row][col_len - 1] == 1:
-                visited[row][col_len - 1] = True
-                queue.append((row, col_len - 1))
-        for col in range(1, col_len - 1):
-            if grid[0][col] == 1:
-                visited[0][col] = True
-                queue.append((0, col))
-            if grid[row_len - 1][col] == 1:
-                visited[row_len - 1][col] = True
-                queue.append((row_len - 1, col))
-        self.bfs(grid, queue, visited)
-        for row in range(row_len):
-            for col in range(col_len):
-                if grid[row][col] == 1 and not visited[row][col]:
-                    ans += 1
-        return ans
+    def numEnclaves(self, matrix: List[List[int]]) -> int:
+      WATER = 0
+      LAND = 1
+      VISITED_ESCAPE = 2
+
+      NUM_ROWS = len(matrix)
+      NUM_COLS = len(matrix[0])
+
+      # runtime: O(m * n) m = rows and n = cols
+      # additional space: O(m * n)
+      def dfs(i, j):
+        matrix[i][j] = VISITED_ESCAPE
+
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        for direction in directions:
+          new_i = i + direction[0]
+          new_j = j + direction[1]
+          # not within matrix boundaries or water or already visited and can escape
+          if new_i < 0 or new_i >= NUM_ROWS or new_j < 0 or new_j >= NUM_COLS or matrix[new_i][new_j] == WATER or matrix[new_i][new_j] == VISITED_ESCAPE:
+            continue
+
+          dfs(new_i, new_j)
+
+        return
+
+      # mark cells as visited escape or visited cannot escape
+      for i in range(NUM_ROWS):
+        if matrix[i][0] == LAND:
+          dfs(i, 0)
+        if matrix[i][NUM_COLS - 1] == LAND:
+          dfs(i, NUM_COLS - 1)
+
+      for j in range(NUM_COLS):
+        if matrix[0][j] == LAND:
+          dfs(0, j)
+        if matrix[NUM_ROWS - 1][j] == LAND:
+          dfs(NUM_ROWS - 1, j)
+
+
+
+      print(matrix)
+      # count how many cannot escape
+      count = 0
+      for i in range(NUM_ROWS):
+        for j in range(NUM_COLS):
+          if matrix[i][j] == LAND:
+            count += 1
+
+      return count
