5883d0af1b95f225c39a771c3f729d344b7ee2bb
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-12 08:50:16 +0800

    Time: 45 ms (86.32%), Space: 17.8 MB (13.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,11 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        ref = {}
+        ref = [0] * 26
         for i in s:
-            ref[i] = ref.get(i, 0) + 1
+            ref[ord(i) - ord('a')] += 1
         for j in t:
-            if j not in ref or ref[j] <= 0:
+            ref[ord(j) - ord('a')] -= 1
+        for k in ref:
+            if k < 0:
                 return False
-            ref[j] -= 1
         return True
