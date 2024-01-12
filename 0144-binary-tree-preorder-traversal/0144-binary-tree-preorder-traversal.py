ea6ea28c7a78ed34846bc15584f9b2af8654f08e
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-12 08:52:57 +0800

    Time: 34 ms (81.76%), Space: 17.2 MB (26.59%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,6 +6,6 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
+        if not root:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
