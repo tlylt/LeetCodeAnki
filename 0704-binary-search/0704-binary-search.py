a940e57f868c4cbde2cd945950bae641a07d7076
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-14 09:11:42 +0800

    Time: 189 ms (93.68%), Space: 19 MB (10.38%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -4,7 +4,7 @@ class Solution:
     def binary(self, l, r, nums, target):
         if r < l:
             return -1
-        mid = l + (r-l)//2
+        mid = l + (r-l) // 2
         if nums[mid] == target:
             return mid
         elif nums[mid] < target:
