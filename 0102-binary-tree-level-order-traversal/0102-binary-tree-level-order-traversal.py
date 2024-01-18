97e97a82e04df561a524f11b35bef3a14307db68
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-19 07:36:52 +0800

    Time: 35 ms (93.81%), Space: 18.1 MB (33.30%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -11,14 +11,13 @@ class Solution:
         q = deque([root])
         ans = []
         while q:
-            l = len(q)
-            level = []
-            for i in range(l):
+            ref = []
+            for i in range(len(q)):
                 curr = q.popleft()
-                level.append(curr.val)
                 if curr.left:
                     q.append(curr.left)
                 if curr.right:
                     q.append(curr.right)
-            ans.append(level)
+                ref.append(curr.val)
+            ans.append(ref)
         return ans
