013b350a880d5d9e7fb186f5d50186f012790547
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-23 07:12:53 +0800

    Time: 3 ms (87.67%), Space: 2.8 MB (83.56%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.go
+++ 1002-find-common-characters/1002-find-common-characters.go
@@ -1,29 +1,25 @@
 func commonChars(words []string) []string {
-    if len(words) == 0 {
-        return []string{}
+    ref := make([]int, 26)
+    for _, char := range words[0] {
+        ref[char-'a']++
     }
-    ref := [26]int{}
-    for _, c := range words[0] {
-        ref[c - 'a']++
-    }
-    
-    for j := 1; j < len(words); j++ {
-        ref2 := [26]int{}
-        for _, k := range words[j] {
-            ref2[k-'a']++
+    var ans []string
+    for i := 1; i < len(words); i++ {
+        ref2 := make([]int, 26)
+        w := words[i]
+        for _, char := range w {
+            ref2[char-'a']++
         }
-        for x:=0; x < 26; x++ {
-            if ref[x] > ref2[x] {
-                ref[x] = ref2[x]
+        for idx, val := range ref {
+            if val > ref2[idx] {
+                ref[idx] = ref2[idx]
             }
         }
     }
-    
-    var ans []string
-    for y := 0; y < 26; y++ {
-        for ref[y] > 0 {
-            ans = append(ans, string('a' + y))
-            ref[y]--
+    for idx, val := range ref {
+        for val > 0 {
+            ans = append(ans, string('a' + idx))
+            val--
         }
     }
     return ans
