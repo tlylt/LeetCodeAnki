ffc4a9c8efcf71d06f16666dabe30c66ff184c8f
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-22 08:11:19 +0800

    Time: 1 ms (84.62%), Space: 2 MB (35.52%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.go
@@ -0,0 +1,12 @@
+func fib(n int) int {
+    if n <= 1 {
+        return n
+    }
+    a, b, c := 0, 1, 0
+    for i := 2; i <= n; i++ {
+        c = a + b
+        a = b
+        b = c
+    }
+    return b
+}
