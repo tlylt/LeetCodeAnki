5300fa1b8eb66b6a5282db575c4f32715224ab91
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-01-27 16:31:00 +0800

    Time: 59 ms (31.85%), Space: 16.9 MB (55.96%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -2,17 +2,17 @@ class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
         ref = [0] * 26
         for i in words[0]:
-            ref[ord(i)-ord('a')] += 1
+            ref[ord(i) - ord('a')] += 1
         for j in range(1, len(words)):
-            ref2 = [0] * 26
             w = words[j]
+            ref2 = [0] * 26
             for k in w:
-                ref2[ord(k)-ord('a')] += 1
-            for z in range(26):
-                ref[z] = min(ref[z], ref2[z])
+                ref2[ord(k) - ord('a')] += 1
+            for x in range(26):
+                ref[x] = min(ref[x], ref2[x])
         ans = []
         for y in range(26):
             while ref[y] > 0:
                 ans.append(chr(y + ord('a')))
-                ref[y]-=1
+                ref[y] -= 1
         return ans
