54e779cd0605111b42e8479cba034d2308373e65
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-04-13 13:41:25 +0800

    Time: 870 ms (87.27%), Space: 31.2 MB (56.48%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,11 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # stack [73, 74, 75] decreasing towards the top
+        # compare stack top vs curr
+        # if curr is bigger than top, pop top and update
         stack = [0]
         ans = [0] * len(temperatures)
         for i in range(1, len(temperatures)):
-            curr = temperatures[i]
-            while stack and curr > temperatures[stack[-1]]:
-                idx = stack.pop()
-                ans[idx] = i - idx
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                ans[stack[-1]] = i - stack[-1]
+                stack.pop()
             stack.append(i)
-        return ans        
+        return ans
+        
+        
