8a97a93182b07a9995ae32c32287f459eaa502b9
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-04-19 07:46:37 +0800

    Time: 25 ms (97.67%), Space: 16.5 MB (26.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,7 +4,7 @@ class Solution:
             return n
         dp = [0, 1]
         for i in range(2, n+1):
-            temp = dp[0] + dp[1]
+            new = dp[0] + dp[1]
             dp[0] = dp[1]
-            dp[1] = temp
+            dp[1] = new
         return dp[1]
