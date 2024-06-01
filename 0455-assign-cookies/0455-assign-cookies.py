f169ea6d618357c2ccd2ea48f176da3ebf1daf82
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2024-06-01 11:21:57 +0800

    Time: 126 ms (87.86%), Space: 18.4 MB (83.32%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -3,13 +3,13 @@ class Solution:
         g.sort()
         s.sort()
         ans = 0
-        g_idx = 0
-        s_idx = 0
-        while g_idx < len(g) and s_idx < len(s):
-            if s[s_idx] >= g[g_idx]:
-                ans += 1
-                g_idx += 1
-                s_idx += 1
-            else:
-                s_idx += 1
+        pt = 0
+        for i in g:
+            while pt < len(s) and s[pt] < i:
+                pt += 1
+            if pt == len(s):
+                return ans
+            ans += 1
+            pt += 1
         return ans
+            
